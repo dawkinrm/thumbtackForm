62bd76c20ebe3c4fd88a9acd3c516e21c330aa05
dawkinrm@dukes.jmu.edu <dawkinrm@dukes.jmu.edu>
2013-03-18 22:06:17 -0400

    Edited the form and added the css file

--- confirmCheck.js
+++ confirmCheck.js
@@ -3,31 +3,45 @@ function validateForm()
 var a=document.forms["signup"]["username"].value;
 var b=document.forms["signup"]["pass"].value;
 var c=document.forms["signup"]["passTwo"].value;
-var x=document.forms["signup"]["email"].value;
-var atpos=x.indexOf("@");
-var dotpos=x.lastIndexOf(".");
+var d=document.forms["signup"]["email"].value;
+var atpos=d.indexOf("@");
+var dotpos=d.lastIndexOf(".");
+
+var x=a.search(/\s/);
 
 if (a==null || a=="")
-  {
-  alert("Username is required!");
-  return false;
-  }
+	{
+		alert("Username is required!");
+		return false;
+	}
+	
+if (x > -1)
+	{
+		alert("Not a valid username!");
+		return false;
+	}
   
 if (b==null || b=="")
-  {
-  	alert ("Password is required!");
-  	return false;
-  }
+	{
+  		alert ("Password is required!");
+  		return false;
+	}
 
-if ( c==null || c=="" || c!=b )
-  {
-  	alert ("Passwords do not match!");
-  	return false;
-  }
+if (c==null || c=="" || c!=b)
+	{
+  		alert ("Passwords do not match!");
+  		return false;
+	}
+  
+if (d==null || d=="")
+	{
+		alert ("E-mail is required!");
+		return false;
+	}	
   
 if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
-  {
-  alert("Not a valid e-mail address");
-  return false;
-  }
+	{
+  		alert("Not a valid e-mail address");
+  		return false;
+	}
 }
